21f7984b928443044443e33083d0b4e8313dac89
Carsten Przyluczky <carsten.przyluczky@9elements.com>
2015-08-25 15:16:25 +0200

    square overlay is placed correctly now

--- imglyKit/Frontend/Camera/IMGLYCameraController.swift
+++ imglyKit/Frontend/Camera/IMGLYCameraController.swift
@@ -106,6 +106,7 @@ public class IMGLYCameraController: NSObject {
     private var setupComplete = false
     private var videoPreviewFrame = CGRectZero
     private let focusIndicatorLayer = CALayer()
+    private let squareIndicatorLayer = CALayer()
     private var focusIndicatorFadeOutTimer: NSTimer?
     private var focusIndicatorAnimating = false
     private let motionManager: CMMotionManager = {
@@ -280,6 +281,28 @@ public class IMGLYCameraController: NSObject {
                 self.delegate?.cameraController?(self, didSwitchToCameraPosition: nextPosition)
             }
         }
+    }
+    
+    // MARK: - Square layer
+    private func setupSquareIndicator() {
+        squareIndicatorLayer.borderColor = UIColor.redColor().CGColor
+        squareIndicatorLayer.borderWidth = 1
+        squareIndicatorLayer.frame.origin = CGPointMake(100, 100)
+        squareIndicatorLayer.frame.size = CGSize(width: kIMGLYIndicatorSize, height: kIMGLYIndicatorSize)
+        squareIndicatorLayer.hidden = false
+        squareIndicatorLayer.anchorPoint = CGPoint(x: 0.5, y: 0.5)
+        previewView.layer.addSublayer(squareIndicatorLayer)
+    }
+    
+    private func updateSquareIndicatorView(newRect: CGRect) {
+        var width = newRect.size.height / 2.0
+        var height = width
+        var top = newRect.origin.x + ((newRect.size.width / 2.0) - width) / 2.0
+        var left = newRect.origin.y / 2.0
+        CATransaction.begin()
+        squareIndicatorLayer.frame = CGRectMake(left, top, width, height)
+        squareIndicatorLayer.needsLayout()
+        CATransaction.commit()
     }
     
     // MARK: - Flash
@@ -633,6 +656,7 @@ public class IMGLYCameraController: NSObject {
         }
         
         setupFocusIndicator()
+        setupSquareIndicator();
         
         setupComplete = true
     }
@@ -1185,7 +1209,7 @@ extension IMGLYCameraController: AVCaptureVideoDataOutputSampleBufferDelegate, A
             
             videoPreviewFrame = sourceExtent
             videoPreviewFrame.fittedIntoTargetRect(targetRect, withContentMode: .ScaleAspectFit)
-            
+            updateSquareIndicatorView(self.videoPreviewFrame)
             if glContext != EAGLContext.currentContext() {
                 EAGLContext.setCurrentContext(glContext)
             }
